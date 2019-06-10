5625f104571fddade1a0777ee3e0c3297ec94a9e
Ian Baker <admin@ianbaker.me>
2019-06-10 16:53:17 +0100

    Adding an ssh server

---
+++ site/profile/manifests/ssh.pp
@@ -0,0 +1,17 @@
+class profile::ssh {
+	package {'openssh-server':
+		ensure => present,
+	}
+	service { 'sshd':
+		ensure => 'running',
+		enable => 'true',
+	}
+	ssh_authorized_key { 'root@master.puppet.vm':
+		ensure => present,
+		user   => 'root',
+		type   => 'ssh-rsa',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCzqCYTG7UH4I5Ou24kkQ6ggPAp0RyiqQtWnwEjUomoNHAn+lzuqnbiZVHeAuiCuOzGpUvEkk5foZnreauNEgWod067uhtFSA
+tYzdanJeu4sylkIKEmZhTJzVjev8K5R7aPzrL6V7ebxZSHtcJ89BQoLvIJ0msXOBIAgZpGlQrEtNQYI4NeOT+JSNtpN+T+Aw7yVY6ionwncNOLeJn6Hnd6UVqTvWMmLl3C89Bf
+WKlvt4/cxSwV99L39ev1uJraSJjpZL/1e88owNsDVc8AHX2fKYHtKY9T0tinPy6+mEkdKZdX+uHG6C9oePrcaU40ggxK/u6imqLCV2rp67ZBVDCr',
+	}  
+}
